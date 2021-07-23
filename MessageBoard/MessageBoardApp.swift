db977b1888d12433964c0ea1ef58bb4a24179336
ginosesia <sesiagino@gmail.com>
2021-07-23 16:51:05 +0100

    Initial Commit

---
+++ MessageBoard/MessageBoardApp.swift
@@ -0,0 +1,17 @@
+//
+//  MessageBoardApp.swift
+//  MessageBoard
+//
+//  Created by Gino Sesia on 23/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct MessageBoardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
